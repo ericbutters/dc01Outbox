ae16ee5e7f37ef7cf5a6f2f0a91ea123ffe944af
ericbutters <dakuit00@googlemail.com>
2011-06-26 22:28:34 +0200

    Added select_tag to use users email

--- app/controllers/outbox_controller.rb
+++ app/controllers/outbox_controller.rb
@@ -1,11 +1,18 @@
 class OutboxController < ApplicationController
  
  def index
+  @users = User.all();
+  
+   respond_to do |format|
+      format.html # index.html.erb
+      format.json { render :json => @users }  
+   end
  end
 
  def send_mail
-   UserMailer.sendme(params[:receiveraddr],params[:subject],params[:message]).deliver
-   @receiver = params[:receiveraddr]
+   user = User.find(params[:userid])
+   UserMailer.sendme(user.email,params[:subject],params[:message]).deliver
+   @receiver = user.email
    @subject = params[:subject]
  end
 
